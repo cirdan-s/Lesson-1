e01a6231c67af8ca6f398a44a5ebd8a616bda41d
Anatolii Palnov <palnov@gmail.com>
2018-10-05 12:18:25 +0300

    Task 3
    Task 3 is completed

--- Task3/src/TestClass.java
+++ Task3/src/TestClass.java
@@ -1,31 +1,29 @@
 import com.sun.tools.javac.util.Assert;
 import org.junit.Test;
 
-public class MainClassTest extends MainClass {
+public class TestClass extends MainClass {
 
     @Test
     public void testGetClassString() {
         String gotString = super.getClassString();
         char firstSymbol = gotString.charAt(0);
-        boolean result = false;
 
-        //System.out.println(firstSymbol);
+     /*   System.out.println(firstSymbol);
+        System.out.println(gotString); */
 
         if (firstSymbol == ('H')) {
             if (gotString.contains("ello")) {
                 System.out.println("Your string starts from \"hello\" or \"Hello\": " + gotString);
-                result = true;
-            }
+             }
+             else Assert.error("Seems that your string does not start from \"hello\" or \"Hello\": " + gotString);
         }
-        if (firstSymbol == ('h')) {
+        else if (firstSymbol == ('h')) {
             if (gotString.contains("ello")) {
                 System.out.println("Your string starts from \"hello\" or \"Hello\": " + gotString);
-                result = true;
-            }
+             }
+             else Assert.error("Seems that your string does not start from \"hello\" or \"Hello\": " + gotString);
         }
-        if (result == false) {
-            Assert.error("Seems that your string does not start from \"hello\" or \"Hello\": " + gotString);
-        }
+        else Assert.error("Seems that your string does not start from \"hello\" or \"Hello\": " + gotString);
 
         System.out.println("==================");
         System.out.println("The test has ended");
